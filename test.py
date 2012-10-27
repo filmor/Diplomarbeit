ac535fb0c7e51fab5c262a2c11a3b20cfda5c7bf
Benedikt Sauer <filmor@gmail.com>
2012-10-28 01:06:08 +0200

    Ganz viel erweitert.

---
+++ test.py
@@ -0,0 +1,26 @@
+from __future__ import division
+from sympy import *
+
+x, l, z = symbols("x lambda z", real=True, positive=True)
+xi = symbols("xi", real=True)
+V, W = map(Function, "VW")
+
+a_2_func = Lambda((x, xi, l, z), xi**2 + l**2 * V(x) + z**2)
+a_0_func = Lambda((x, xi, l, z), W(x))
+
+def D(f, x, n=1):
+    return (-I)**n * f.diff(x, n)
+
+def r(n, a_2=Symbol("a_2"), a_0=Symbol("a_0")):
+    if n < 2:
+        return Lambda((x, xi, l, z), 0)
+    if n == 2:
+        return a_2(x,xi,l,z) ** (-1)
+
+    rec = lambda k: r(n-k, a_2, a_0)
+    return a_2(x, xi, l, z) ** (-1) * (
+                  2 * xi * I * D(rec(1), x)
+                + 2 * D(rec(2), x, 2)
+                - (a_0(x, xi, l, z) * rec(2) if n > 3 else 0)
+                )
+
